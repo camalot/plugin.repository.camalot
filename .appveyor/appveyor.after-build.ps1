f77e6c160f218e3cfc345d2e03b42286132fb3a6
camalot <camalot@gmail.com>
2016-11-26 20:17:28 -0600

    update after-build script

--- .appveyor/appveyor.after-build.ps1
+++ .appveyor/appveyor.after-build.ps1
@@ -1,12 +1,10 @@
 if ( $env:APPVEYOR_REPO_BRANCH -eq "master" ) {
-  # $env:CI_DEPLOY_GITHUB = $true;
-  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB", "$true", "Machine")
+  $env:CI_DEPLOY_GITHUB = $true;
+  $env:CI_DEPLOY_GITHUB_PRE = $false;
 } elseif ( $env:APPVEYOR_REPO_BRANCH -eq "develop" ) {
-  # $env:CI_DEPLOY_GITHUB_PRE = $true;
-  # $env:CI_DEPLOY_GITHUB = $false;
-  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB_PRE", "$true", "Machine")
-  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB", "$false", "Machine")
+  $env:CI_DEPLOY_GITHUB_PRE = $true;
+  $env:CI_DEPLOY_GITHUB = $false;
 } else {
-  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB_PRE", "$false", "Machine")
-  [Environment]::SetEnvironmentVariable("CI_DEPLOY_GITHUB", "$false", "Machine")
+  $env:CI_DEPLOY_GITHUB_PRE = $false;
+  $env:CI_DEPLOY_GITHUB = $false;
 }
